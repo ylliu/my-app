1e0cd40ae9ede41938d6882d77d9ffe798c504ca
ylliu <liuming1908@gmail.com>
2022-09-28 07:27:57 +0800

    test 2: add display name input

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -5,6 +5,9 @@ export class UserSignupPage extends React.Component {
         return (
             <div>
                 <h1>Sign Up</h1>
+                <div>
+                    <input placeholder="Your display name" />
+                </div>
             </div>
         )
     }
--- src/pages/UserSignupPage.spec.js
+++ src/pages/UserSignupPage.spec.js
@@ -3,12 +3,18 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { UserSignupPage } from './UserSignupPage';
 
-describe('UserSignupPage',()=>{
-    describe('Layout',()=>{
-        it('has header of Sign up',()=>{
+describe('UserSignupPage', () => {
+    describe('Layout', () => {
+        it('has header of Sign up', () => {
             const { container } = render(<UserSignupPage />)
             const header = container.querySelector('h1');
             expect(header).toHaveTextContent('Sign Up');
-        })
+        });
+
+        it('has input for dispaly name', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />);
+            const dispalyNameInput = queryByPlaceholderText('Your display name');
+            expect(dispalyNameInput).toBeInTheDocument();
+        });
     })
 })
