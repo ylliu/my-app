b30331cebc0c62e0514a24cae39ee278d9a2d0e1
ylliu <liuming1908@gmail.com>
2022-10-01 09:51:17 +0800

    sending request to backend

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserSignupPage } from './pages/UserSignupPage'
+import { UserSignupPage } from './pages/UserSignupPage';
+import * as apiCalls from './api/apiCalls';
+
+const actions = {
+  postSignup: apiCalls.signup
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserSignupPage />
+    <UserSignupPage actions={actions} />
   </React.StrictMode>
 );
 
